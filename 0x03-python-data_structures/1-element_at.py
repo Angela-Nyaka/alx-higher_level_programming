1c65ada559b8db7921b1de04d31afbb366f4a69b
Angela-Nyaka <nyakaangela@gmail.com>
2024-01-08 09:36:24 -0800

    addd task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "main":
+def element_at(my_list, idx):    
+if idx == 0:
+       print ("None")
+    if idx > my_list[i] + 1:
+       print ("None")
+print(my_list.pop)
